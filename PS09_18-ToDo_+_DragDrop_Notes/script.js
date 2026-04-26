a5f4f9666f9d920e92c50aac6c5d546aaf3bc5a4
kaustubhk02 <kaustubhkumbhare05@gmail.com>
2026-04-26 15:59:20 +0530

    Added Notes to local Storage

--- PS09_18-ToDo_+_DragDrop_Notes/script.js
+++ PS09_18-ToDo_+_DragDrop_Notes/script.js
@@ -8,21 +8,34 @@ addbut.addEventListener('click', ()=>{
     // Add task logic from here
 
     let input = document.querySelector('#addtask').value;
+    let input2 = document.querySelector('#description').value;
 
     let li = document.createElement('li');
     li.textContent = input;
 
+    let l2 = document.createElement('li');
+    l2.textContent = input2;
+
+    l2.setAttribute("id", input2);
+    li.setAttribute("id", input)
     li.setAttribute("draggable", "true"); /* Make draggable */
 
     let del = document.createElement('button');
     del.textContent = 'Delete';
 
-    del.onclick = () => li.remove();
+    del.onclick = () => {
+        li.remove();
+        if(localStorage.getItem(input)) localStorage.removeItem(input);
+    }
 
     li.appendChild(del);
+    li.appendChild(l2);
     ul.appendChild(li);
+    
+    localStorage.setItem(input, input2);
 
     document.querySelector('#addtask').value = "";
+    document.querySelector('#description').value = "";
 
     // Drag and Drop from here
 
