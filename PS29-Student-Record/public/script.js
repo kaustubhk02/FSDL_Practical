7f4f5ea4f6665dcb50b0cb46c6eb87a0e00e9a67
kaustubhk02 <kaustubhkumbhare05@gmail.com>
2026-04-26 23:04:24 +0530

    Pracriced

--- PS29-Student-Record/public/script.js
+++ PS29-Student-Record/public/script.js
@@ -1,26 +1,28 @@
-function addStudent(){
-    let data = {
-        name: document.getElementById("name").value,
-        roll: document.getElementById("roll").value,
-        branch: document.getElementById("branch").value
-    };
 
-    fetch("http://localhost:3000/add", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+async function addStudent(){
+    let data = {
+        name: document.getElementById('name').value,
+        roll: document.getElementById('roll').value,
+        branch: document.getElementById('branch').value
+    }
+
+    await fetch("http://localhost:3000/add", {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify(data)
-    }).then(() => alert("Saved"));
+    });
+
+    alert("Saved");
 }
 
-function getStudents(){
-    fetch("http://localhost:3000/students")
-    .then(res => res.json())
-    .then(data => {
-        let list = document.getElementById("list");
-        list.innerHTML = "";
+async function getStudents(){
+    let res = await fetch("http://localhost:3000/students");
+    let data = await res.json();
 
-        data.forEach(s => {
-            list.innerHTML += `<li>${s.name} - ${s.roll} - ${s.branch}</li>`;
-        });
+    let li = document.getElementById('list');
+    li.innerHTML = "";
+
+    data.forEach(s=>{
+        li.innerHTML += `<li>${s.name} - ${s.roll} - ${s.branch}</li>`;
     });
 }
--- PS29-Student-Record/server.js
+++ PS29-Student-Record/server.js
@@ -7,10 +7,10 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 
-// connect MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB'); 
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB').then(()=>{
+    console.log("MongoDB connected.....");
+});
 
-// schema
 const studentSchema = new mongoose.Schema({
     name: String,
     roll: String,
@@ -19,17 +19,18 @@ const studentSchema = new mongoose.Schema({
 
 const Student = mongoose.model('Student', studentSchema, 'students');
 
-// add student
-app.post('/add', async (req, res) => {
+app.post('/add', async (req, res)=>{
     const student = new Student(req.body);
     await student.save();
-    res.send("Student Saved");
+    console.log("New Student added");
+    res.send('Student Saved');
 });
 
-// get students
-app.get('/students', async (req, res) => {
+app.get('/students', async (req, res)=>{
     const data = await Student.find();
     res.json(data);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, ()=>{
+    console.log('Server running on port - 3000');
+});
